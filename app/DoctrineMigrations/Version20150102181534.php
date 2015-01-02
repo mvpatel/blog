aec5192214b55be3bc6fc29bf5c23dda593eeba7
mvpatel <bestmvpatel@gmail.com>
2015-01-02 17:26:30 +0000

    final vesrion 2

--- app/DoctrineMigrations/Version20150102181534.php
+++ app/DoctrineMigrations/Version20150102181534.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20150102031100 extends AbstractMigration
+class Version20150102181534 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
